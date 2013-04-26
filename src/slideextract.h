f751367f28a12afb1438504d2cf6a492a45a765b
Angelo Haller <angelo@szanni.org>
2013-04-26 17:41:30 +0200

    Add basic slide extraction based on comparison region.

---
+++ src/slideextract.h
@@ -0,0 +1,51 @@
+/* slideextract -- api header
+ *
+ * Copyright (c) 2013, Angelo Haller <angelo@szanni.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef SLIDEEXTRACT_H
+#define SLIDEEXTRACT_H
+
+#include <opencv2/core/types_c.h>
+
+/*!
+ * \brief Playback video for roi selection with mouse
+ * \param file Video file
+ * \param roi Region of interest selected by user
+ * \return 0 on success, -1 on failure or abort by user
+ */
+int se_select_roi (const char *file, CvRect *roi);
+
+/*!
+ * \brief Extract slides from video
+ * \param file Video file
+ * \param outprefix Output filename prefix
+ * \param roi Frame comparison region for slide detection, whole frame on NULL
+ * \return 0 on success, -1 on failure
+ */
+int se_extract_slides (const char *file, const char *outprefix, CvRect *roi);
+
+#endif
+
