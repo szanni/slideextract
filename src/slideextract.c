7e5b01b60e6ceb9a4321b9189dbf2014cb18480e
Angelo Haller <angelo@szanni.org>
2017-11-02 16:29:32 +0100

    Fix include issues relating to the opencv 2 library.
    This seems to be an upstream issue in the opencv 2 library bindings for C.
    The *highgui_c.h* file does not correctly include all necessary dependencies,
    this here is just a quick fix until the issue is resolved upstream.

--- src/slideextract.c
+++ src/slideextract.c
@@ -27,8 +27,11 @@
 
 #include "slideextract.h"
 #include <stdio.h>
+#include <opencv2/core/core_c.h>
 #include <opencv2/highgui/highgui_c.h>
 #include <opencv2/imgproc/imgproc_c.h>
+#include <opencv2/imgcodecs/imgcodecs_c.h>
+#include <opencv2/videoio/videoio_c.h>
 
 static _Bool selected_roi = 0;
 static CvPoint point1;
